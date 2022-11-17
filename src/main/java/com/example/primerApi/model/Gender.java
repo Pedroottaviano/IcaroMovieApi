dab5d803b3a641ec86d4e7a5b468a59fe2104b3e
pedro-ottaviano-globant <pedro.ottaviano@globant.com>
2022-11-17 10:25:46 -0300

    enum added

--- src/main/java/com/example/primerApi/model/Gender.java
+++ src/main/java/com/example/primerApi/model/Gender.java
@@ -1,11 +1,18 @@
 package com.example.primerApi.model;
 
 public enum Gender {
-    Acción,
-    Suspenso,
-    Terror,
-    Romantico,
-    Policial,
-    Documental,
-    Musical;
+    ACCION("Acción"),
+    POLICIAL("Policial"),
+    ROMANTICA("Romantica"),
+    DOCUMENTAL("Documental");
+    private final String gender;
+
+    Gender(String gender){
+        this.gender = gender;
+    }
+
+    public String getGender(){
+        return this.gender;
+    }
+
 }
